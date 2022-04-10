36e8b56f3ffbcb38cd980872c7c5bd66696482d1
aa1381k <a.abdolo.k81@gmail.com>
2022-04-10 19:12:24 +0430

    V1

---
+++ backend/resume_website/account_module/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+
+
+class Register_form(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'email border'}),validators=[validators.EmailValidator])
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'password border','id':'password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'confirm-password border','id':'confirm-password'}))
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+
+        if password == confirm_password:
+            return confirm_password
+
+        raise ValidationError('کلمه عبور و تکرار کلمه عبور مغایرت دارند')
+
+
+class Login_form(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'email border'}),validators=[validators.EmailValidator])
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'password border','id':'password'}))
